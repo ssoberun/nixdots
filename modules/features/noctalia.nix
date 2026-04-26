ed8b6fe11e2b2a5505aa5a79decb444744102498
ssoberun <5744sam@gmail.com>
2026-04-26 21:01:11 +1000

    added back parts.nix, oops

--- modules/features/noctalia.nix
+++ modules/features/noctalia.nix
@@ -1,16 +1,14 @@
 { self, inputs, ... }: {
-  # 1. The NixOS Module: This makes the command available system-wide
   flake.nixosModules.noctalia = { pkgs, ... }: {
     environment.systemPackages = [
       self.packages.${pkgs.stdenv.hostPlatform.system}.noctalia-shell
+      # self'.packages.${pkgs.stdenv.hostPlatform.system}.noctalia-shell
     ];
   };
 
-  # 2. The Package Definition: This handles the actual wrapping logic
   perSystem = { pkgs, ... }: {
     packages.noctalia-shell = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
       inherit pkgs;
-      # Using fromJSON as you prefer for your theme/layout
       settings = builtins.fromJSON (builtins.readFile ./noctalia.json);
     };
   };
--- modules/hosts/x1-machine/default.nix
+++ modules/hosts/x1-machine/default.nix
@@ -2,7 +2,6 @@
 	flake.nixosConfigurations.x1-machine = inputs.nixpkgs.lib.nixosSystem {
 		modules = [
 			self.nixosModules.x1-machine-configuration
-			self.nixosModules.myHomeManager
 		];
 	};
 }
