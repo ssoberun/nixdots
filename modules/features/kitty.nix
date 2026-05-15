f2fbb49d04e7fcc9e93488de549446a1055b710c
ssoberun <5744sam@gmail.com>
2026-05-15 22:40:38 +1000

    system upg complete, kitty shit, os feels smoother?

--- modules/features/kitty.nix
+++ modules/features/kitty.nix
@@ -31,6 +31,7 @@
         scrollback_lines = 2000;
         hide_window_decorations = true;
         confirm_os_window_close = 0;
+        pixel_scroll = true;
       };
       extraConfig = ''
         include ~/.config/kitty/themes/noctalia.conf
