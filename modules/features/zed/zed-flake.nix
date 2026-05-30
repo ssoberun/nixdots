f3db914c1dbcccf3566a68ba35117b63903137f1
ssoberun <5744sam@gmail.com>
2026-05-30 11:28:54 +1000

    zed moar

---
+++ modules/features/zed/zed-flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs,
+  ...
+}:
+{
+
+  flake.nixosModules.zed =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    {
+      environment.systemPackages = with pkgs; [
+        zed-editor
+      ];
+
+      hj.xdg.config.files."zed/settings.json" = {
+        generator = lib.generators.toJSON { };
+        value = {
+        };
+      };
+    };
+}
